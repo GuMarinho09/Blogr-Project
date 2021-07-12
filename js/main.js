753e6c7a177a4597c1e760dc0582ea03408cbee6
Gu Marinho <gumarinho09@gmail.com>
2021-07-12 16:52:04 -0300

    Nav Bar. Done.

--- js/main.js
+++ js/main.js
@@ -6,8 +6,39 @@ collapsibles.forEach((item) =>
 );
 
 const collapsiblesHamburger = document.querySelectorAll(".hamburger");
-collapsibles.forEach((item) =>
+collapsiblesHamburger.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("hamburger--closed");
   })
 );
+
+const navItemOne = document.getElementById("nav__item-one");
+const navItemTwo = document.getElementById("nav__item-two");
+const navItemThree = document.getElementById("nav__item-three");
+navItemOne.addEventListener("click", function () {
+    if (navItemTwo.classList.contains("collapsible--expanded")) {
+      navItemTwo.classList.remove("collapsible--expanded");
+    }
+
+    if (navItemThree.classList.contains("collapsible--expanded")) {
+      navItemThree.classList.remove("collapsible--expanded");
+    }
+});
+navItemTwo.addEventListener("click", function () {
+    if (navItemOne.classList.contains("collapsible--expanded")) {
+      navItemOne.classList.remove("collapsible--expanded");
+    }
+
+    if (navItemThree.classList.contains("collapsible--expanded")) {
+      navItemThree.classList.remove("collapsible--expanded");
+    }
+});
+navItemThree.addEventListener("click", function () {
+    if (navItemOne.classList.contains("collapsible--expanded")) {
+      navItemOne.classList.remove("collapsible--expanded");
+    }
+
+    if (navItemTwo.classList.contains("collapsible--expanded")) {
+      navItemTwo.classList.remove("collapsible--expanded");
+    }
+});
